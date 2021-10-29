8b3ae703ddd28640b15b4f58d6bb7afed7474234
rbviz <inbox@rbviz.com>
2021-10-29 12:45:31 +0300

    init

---
+++ lib/action_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'action_job/version'
+
+module ActionJob
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/action_job/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActionJob
+  VERSION = '0.1.0'
+end
